f58303143989f023c42f09dd389b53db121baf00
Tomo Wang <tomo_wang@163.com>
2024-03-06 22:45:33 +0800

    feat: use nginx var to replacement instead of hardcoded hostname

--- civitai_api_transform.lua
+++ civitai_api_transform.lua
@@ -7,8 +7,18 @@ end
 local data = ngx.arg[1]
 local eof = ngx.arg[2]
 local next_idx = ctx.nbuffers + 1
+
+local scheme = ngx.var.scheme
+local host = ngx.var.host
+local port = ngx.var.server_port
 local original_str = "https://civitai.com/api/download/models/(%d+)"
-local replacement = "https://c-mirror.com/api/download/models/%1"
+local replacement
+
+if (scheme == 'http' and port == 80) or (scheme == 'https' and port == 443) then
+  replacement = string.format("%s://%s/api/download/models/%%1", scheme, host)
+else
+  replacement = string.format("%s://%s:%d/api/download/models/%%1", scheme, host, port)
+end
 
 if not eof then
   if data then
